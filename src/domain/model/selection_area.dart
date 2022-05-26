73abddb9f8c09e1930572262c5a6f9d8101f9a36
Tokkyer Midlow <mltokky@gmail.com>
2022-05-27 00:15:53 +0900

    Del: 不要となったコードを削除

--- src/domain/model/selection_area.dart
+++
@@ -1,9 +0,0 @@
-class SelectionArea {
-  final String prefecture;
-  final String city;
-  final String town;
-  final double x;
-  final double y;
-
-  SelectionArea(this.prefecture, this.city, this.town, this.x, this.y);
-}
